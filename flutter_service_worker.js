1b370a5002d29925f5c9d2bed5fa64645cde3651
Smeetsv10 <victorsmeets.vs@gmail.com>
2026-01-07 21:35:01 +0100

    latest version after fixing commits

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "1c849ea09a9ee8cbdf9c6d3bc8194504
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "c310456e00bdad37e0cc97d47ac97b76",
+"flutter_bootstrap.js": "f2f0e22e4c9c1ba86106e2bdacc35f53",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
